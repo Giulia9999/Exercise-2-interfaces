e1493cd79448c0e1dd3ac2b867eabbf0dcc7042d
Giulia <giuliac35@gmail.com>
2023-01-13 11:39:23 +0100

    Define the testing class Start
    I created 3 objects for each subclass and implemented the methods from the superclass and the interfaces.

--- src/it/develhope/interface1/Start.java
+++ src/it/develhope/interface1/Start.java
@@ -1,4 +1,23 @@
 package it.develhope.interface1;
 
+import it.develhope.interface1.entities.Assistant;
+import it.develhope.interface1.entities.Professor;
+import it.develhope.interface1.entities.Student;
+import jdk.jshell.spi.ExecutionControl;
+
 public class Start {
+    public static void main(String[] args){
+        Student student = new Student("Giulia", "Conta", 234, 2022);
+        Professor professor = new Professor("Paolo", "Licata", 567, "Computer science");
+        Assistant assistant = new Assistant("Marco", "Rossi", 890, true);
+
+        student.goToCollege();
+        professor.goToCollege();
+        assistant.goToCollege();
+
+        student.studyAtHome();
+        professor.teachToOtherPeople();
+        assistant.studyAtHome();
+        assistant.teachToOtherPeople();
+    }
 }
